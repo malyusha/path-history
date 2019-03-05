b13ea3c09c55f80b03ccc328ae2c09d659d82d2e
malyusha <im@kodix.ru>
2019-03-05 18:41:18 +0300

    Removed unused example

--- examples/configuration/path_history.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * This file is a part of Kodix Homer project.
- * Email:       support@kodix.ru
- * Company:     Kodix LLC <https://kodix.com>
- * Developer:   Igor Malyuk <https://github.com/malyusha>
- */
-
-return [];
